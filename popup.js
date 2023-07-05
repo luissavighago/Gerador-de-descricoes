92fc8fc5d453a97cca19e50965eb5c38cbb043af
luissavighago <55593826+luissavighago@users.noreply.github.com>
2023-07-05 19:29:32 -0300

    #styles e copia para clipboard

--- popup.js
+++ popup.js
@@ -26,3 +26,21 @@ document.addEventListener("DOMContentLoaded",() => {
     })
   });
 });
+
+document.getElementById("form").onsubmit = (event) =>{
+  let taskId = document.getElementById("input-task").value;
+  let subtaskId = document.getElementById("select-subtasks").value;
+  let version = document.getElementById("input-version").value;
+  let title = document.getElementById("input-commit-title").value;
+
+  let commitTitle = `${taskId != null && taskId.length > 0 ? "#"+taskId.trim():""} ${subtaskId != null && subtaskId.length > 0 ? "#"+subtaskId.trim() : "" } ${version.trim()} ${title.trim()}`
+
+  navigator.clipboard.writeText(commitTitle).then(() => {
+      console.log("Texto copiado para o clipboard: ", commitTitle);
+    }).catch((error) => {
+      console.error("Falha ao copiar para o clipboard: ", error);
+    });
+  
+  // Evitar o envio do formulário
+  event.preventDefault();
+}
