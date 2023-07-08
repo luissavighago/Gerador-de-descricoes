a7aeea0ebb47755c6ae5f5b4d38ec80dd0562916
luissavighago <55593826+luissavighago@users.noreply.github.com>
2023-07-07 22:14:42 -0300

    #ajuste ao buscar subtarefa

--- contentScript.js
+++ contentScript.js
@@ -21,13 +21,17 @@ const getTitle = () => {
 const getSubtasks = () => {
   let elements = []
   let subTasksContainer = document.querySelector("#ak-main-content > div > div > div._4t3i1osq._1e0c1txw._2lx21bp4 > div._4t3i1osq._kqswh2mm > div._kqswh2mm._4t3i1osq > div._ogwtidpf._6tinidpf._1cezidpf._m3zkidpf._7yjtidpf._ldgnidpf._un3pidpf._29hzidpf._4t3i1osq._1e0c1txw._2lx21bp4._15y61q9c._k8em1osq._dzc24jg8 > div > div._12ji1r31._1qu2glyw._12y31o36._1reo1e7b._18m92qvy._16jlkb7n._1o9zidpf._i0dlidpf._1ul9xilx._19bv18bx > div > div._19pkftgi > div:nth-child(3) > div > div._19pkftgi._otyrftgi._u5f3t94y")
-  let subTasksList = subTasksContainer.querySelector("#ak-main-content > div > div > div._4t3i1osq._1e0c1txw._2lx21bp4 > div._4t3i1osq._kqswh2mm > div._kqswh2mm._4t3i1osq > div._ogwtidpf._6tinidpf._1cezidpf._m3zkidpf._7yjtidpf._ldgnidpf._un3pidpf._29hzidpf._4t3i1osq._1e0c1txw._2lx21bp4._15y61q9c._k8em1osq._dzc24jg8 > div > div._12ji1r31._1qu2glyw._12y31o36._1reo1e7b._18m92qvy._16jlkb7n._1o9zidpf._i0dlidpf._1ul9xilx._19bv18bx > div > div._19pkftgi > div:nth-child(3) > div > div._19pkftgi._otyrftgi._u5f3t94y > ul ")
-  Array.from(subTasksList.querySelectorAll('[data-test-id="issue.issue-view.views.common.issue-line-card.issue-line-card-view.key"]')).forEach(element => {
-    let value = element.textContent;
-    if (value != null && value != undefined) {
-      elements.push(value.trim());
+  if(subTasksContainer != null){
+    let subTasksList = subTasksContainer.querySelector("#ak-main-content > div > div > div._4t3i1osq._1e0c1txw._2lx21bp4 > div._4t3i1osq._kqswh2mm > div._kqswh2mm._4t3i1osq > div._ogwtidpf._6tinidpf._1cezidpf._m3zkidpf._7yjtidpf._ldgnidpf._un3pidpf._29hzidpf._4t3i1osq._1e0c1txw._2lx21bp4._15y61q9c._k8em1osq._dzc24jg8 > div > div._12ji1r31._1qu2glyw._12y31o36._1reo1e7b._18m92qvy._16jlkb7n._1o9zidpf._i0dlidpf._1ul9xilx._19bv18bx > div > div._19pkftgi > div:nth-child(3) > div > div._19pkftgi._otyrftgi._u5f3t94y > ul ")
+    if(subTasksList != null && subTasksList.length > 0){
+      Array.from(subTasksList.querySelectorAll('[data-test-id="issue.issue-view.views.common.issue-line-card.issue-line-card-view.key"]')).forEach(element => {
+        let value = element.textContent;
+        if (value != null && value != undefined) {
+          elements.push(value.trim());
+        }
+      });
     }
-  });
+  }
   return elements;
 }
 
